cadf232cef561ae7385445e5427dba980dac3976
Spless01 <sples.zero@gmail.com>
2024-12-25 15:22:10 +0100

    del

--- knowledge_site — копия 2/knowledge/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from knowledge import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('articles/', views.search_articles, name='search_articles'),
-    path('vidannya/', views.search_vidannya, name='search_vidannya'),
-
-]
